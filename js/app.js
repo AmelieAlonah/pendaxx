b0740a521d834e04a9ce05977f920d00cc3d9e67
AmelieAlonah <amelie.abdallah@hotmail.fr>
2021-05-02 18:32:08 +0200

    changement de theme OK

--- js/app.js
+++ js/app.js
@@ -1,11 +1,25 @@
 const app = {
+
     init : function(){
         let button = document.querySelector('#button') ;
         button.addEventListener('click', game.handleCheckLetter) ;
 
         let seasonSelect = document.querySelector('#season');
         seasonSelect.addEventListener('change', game.choiceSeason) ;
+
+        // Ceci est pour le changement de theme :
+        let themes = document.querySelector('#themes') ;
+        themes.addEventListener('change', app.handleThemeSelect ) ;
         
+    },
+
+    //Changement de thème de couleur
+    handleThemeSelect : function( event ) {
+
+        let valueTheme = document.querySelector('#themes').value ;
+        document.querySelector('body').className = valueTheme ;
+        let themeBody = document.querySelector('body') ;
+        themeBody.classList.add( valueTheme ) ;
     },
 
    
